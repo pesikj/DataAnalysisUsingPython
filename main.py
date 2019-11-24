e0531baf4f70d826db73120df2c90cd247b5657f
pesikj <jiri.pesik@outlook.cz>
2019-11-24 20:55:34 +0100

    Update of colours

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from statistical_hypothesis_testing.plots import plots_z_test
 from statistical_hypothesis_testing.tails import Tail
 
 
-plots_z_test.create_p_value_plot(-0.2269, alpha=0.05, tails=Tail.RIGHT_TAILED)
+#plots_z_test.create_critical_region_plot(alphas=[0.1, 0.05, 0.01])
+plots_z_test.create_p_value_plot(-1.2125,alpha=0.05,lang='cs')
--- statistical_hypothesis_testing/plots/plots_z_test.py
+++ statistical_hypothesis_testing/plots/plots_z_test.py
@@ -11,7 +11,7 @@ def create_critical_region_plot(alphas=[0.05, ], tails=Tail.TWO_TAILED, x_min=-3
     alphas = sorted(alphas, reverse=True)
     x_max = x_min * (-1)
 
-    red = Color("red")
+    red = Color("#FF7F50")
     colors = list(red.range_to(Color("white"),len(alphas)+1))
     alphas_with_colors = [(alphas[i], colors[-i-2]) for i in range(0, len(alphas))] 
 
@@ -50,13 +50,13 @@ def create_critical_region_plot(alphas=[0.05, ], tails=Tail.TWO_TAILED, x_min=-3
     plt.xlim(x_min,x_max)
     plt.ylim(0,y_max)
 
-    plt.title('Critical Regions for z-test',fontsize=10)
+    plt.title('Kritické obory pro z-test',fontsize=10)
     plt.xlabel('x')
 
     plt.savefig("z_test_critical_region.png")
     plt.show()
 
-def create_p_value_plot(statistics,alpha=0.05, tails=Tail.TWO_TAILED, x_min=-3, y_max=0.5):
+def create_p_value_plot(statistics,alpha=0.05, tails=Tail.TWO_TAILED, x_min=-3, y_max=0.5, lang='en'):
     x_max = x_min * (-1)
 
     x = np.linspace(x_min, x_max, 100)
@@ -75,32 +75,35 @@ def create_p_value_plot(statistics,alpha=0.05, tails=Tail.TWO_TAILED, x_min=-3, 
 
         plt.fill_between(ptx, pty, facecolor=color, alpha='1.0', hatch=hatch, edgecolor=edgecolor)
 
-
-    legend_patches = []
-
     if tails == Tail.TWO_TAILED:
-        generate_area(x_min, norm.ppf(alpha/2), 'red')
-        generate_area(norm.ppf(1 - alpha/2), x_max, 'red')
-        generate_area(x_min, -abs(statistics), 'none', hatch='///', edgecolor='blue')
-        generate_area(abs(statistics), x_max, 'none', hatch='///', edgecolor='blue')
+        generate_area(x_min, norm.ppf(alpha/2), '#FF7F50')
+        generate_area(norm.ppf(1 - alpha/2), x_max, '#FF7F50')
+        generate_area(x_min, -abs(statistics), 'none', hatch='///', edgecolor='#5B84B1FF')
+        generate_area(abs(statistics), x_max, 'none', hatch='///', edgecolor='#5B84B1FF')
     elif tails == Tail.LEFT_TAILED:
-        generate_area(x_min, norm.ppf(alpha), 'red')
-        generate_area(x_min, statistics, 'none', hatch='///', edgecolor='blue')
+        generate_area(x_min, norm.ppf(alpha), '#FF7F50')
+        generate_area(x_min, statistics, 'none', hatch='///', edgecolor='#5B84B1FF')
     elif tails == Tail.RIGHT_TAILED:
-        generate_area(norm.ppf(1 - alpha), x_max, 'red')
-        generate_area(statistics, x_max, 'none', hatch='///', edgecolor='blue')
+        generate_area(norm.ppf(1 - alpha), x_max, '#FF7F50')
+        generate_area(statistics, x_max, 'none', hatch='///', edgecolor='#5B84B1FF')
 
-    legend_patches.append(mpatches.Patch(color='red', label='Critical Region'.format(alpha)))
-    legend_patches.append(mpatches.Patch(color='blue', label='p-value'.format(alpha)))
-
-    plt.legend(handles=legend_patches)
+    
 
     plt.grid()
 
     plt.xlim(x_min,x_max)
     plt.ylim(0,y_max)
 
-    plt.title('p-value of z-test',fontsize=10)
+    legend_patches = []
+    if lang == 'en':
+        plt.title('p-value of z-test',fontsize=10)
+        legend_patches.append(mpatches.Patch(color='#FF7F50', label='Critical Region'.format(alpha)))
+        legend_patches.append(mpatches.Patch(color='#5B84B1FF', label='p-value'.format(alpha)))
+    elif lang == 'cs':
+        plt.title('p-hodnota z-test',fontsize=10)
+        legend_patches.append(mpatches.Patch(color='#FF7F50', label='Kritický obor'.format(alpha)))
+        legend_patches.append(mpatches.Patch(color='#5B84B1FF', label='p-hodnota'.format(alpha)))
+    plt.legend(handles=legend_patches)
     plt.xlabel('x')
 
     plt.savefig("z_test_p_value.png")
